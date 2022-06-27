4211907fd216860fc021d85e9e5a4667cd6f5288
efomc <98823444+efomc@users.noreply.github.com>
2022-06-28 00:03:09 +0400

    Update task2_core.py

--- task2/task2_core.py
+++ task2/task2_core.py
@@ -4,3 +4,12 @@ def int32_to_ip(int32):
         ipv_adress_list.append(str(int32 // 256**octet_exponent))
         int32 = int32 % 256**octet_exponent
     return ".".join(ipv_adress_list)
+
+
+def main():
+    int32 = int(input('Введите, пожалуйста, 32-битное целое число: '))
+    print(int32_to_ip(int32))
+
+
+if __name__ == "__main__":
+    main()
